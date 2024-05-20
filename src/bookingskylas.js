6ea72322b2b07158734bf1a4255783125e8d1b2f
Jismaeel <146990352+Jismaeel@users.noreply.github.com>
2024-05-20 13:18:04 +0200

    ujiko

--- src/bookingskylas.js
+++ src/bookingskylas.js
@@ -18,7 +18,7 @@ app.use(
 );
 
 // POST route for form submission
-app.post('/submit-form', (req, res) => {
+app.post('/bookingsskylas', (req, res) => {
     const { fullname, contact, email, address, courseOption, packageOption, carHire, selectedDate } = req.body;
 
     // Create a transporter with Gmail SMTP
