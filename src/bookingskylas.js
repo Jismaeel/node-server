c4366579dc419f22dc9a29229891d0aed5d73b9c
Jismaeel <146990352+Jismaeel@users.noreply.github.com>
2024-05-22 20:38:55 +0200

    bnm

--- src/bookingskylas.js
+++ src/bookingskylas.js
@@ -18,7 +18,7 @@ app.use(
 );
 
 // POST route for form submission
-app.post('/submit-bookingsskylas', (req, res) => {
+app.post('/submit-bookingsskylas', async (req, res) => {
     const { fullname, contact, email, address, courseOption, packageOption, carHire, selectedDate } = req.body;
 
     // Create a transporter with Gmail SMTP
--- src/contactform.js
+++ src/contactform.js
@@ -17,7 +17,7 @@ app.use(
   })
 );
 // Endpoint to handle form submission
-app.post("/submit-form", (req, res) => {
+app.post("/submit-form", async (req, res) => {
   const { name, email, message } = req.body;
   // Create a transporter with Gmail SMTP
   const transporter = nodemailer.createTransport({
